6b280fab94478bab4b0aa90a060945e27385944c
chenlang <q1025518502@qq.com>
2019-08-19 18:34:48 +0800

    CI,CD学习

---
+++ third/demo1.js
@@ -0,0 +1,10 @@
+/**
+ * CI:持续集成
+ * CD:持续交付
+ * CD:持续部署(较少，非常依赖自动化测试)
+ * devops:运维平台,持续部署平台
+ * CI平台:jenkins(svn)，Travis CI(git),circle CI(最新的)
+ * 花生壳:内网穿透
+ * FIS3:自动化构建工具(webpack/jdists/package.json/chai/supuertest/mocha/selenium-webdriver)
+ * 部署工具:rsync,shelljs,yargs
+ */
